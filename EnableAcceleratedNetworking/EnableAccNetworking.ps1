6f8828075a3d5d4f68085913181975ccc384e7ec
Subhro Majumder <48013811+msubhro@users.noreply.github.com>
2020-06-21 10:59:52 +0530

    Add files via upload

---
+++ EnableAcceleratedNetworking/EnableAccNetworking.ps1
@@ -0,0 +1,60 @@
+﻿# This script Enable Accelered Networking for Azure VM(s).
+
+# VM must be deallocated before running this script.
+
+# VM Size must support Accelerated Networking. Not all Azure VM supports Accelerated Networking.
+
+$VMList = get-content 'c:\serverlist.txt'
+
+$VMRGName = 'VMResourceGroupName'
+
+
+$Jobs = @()
+
+
+
+$sblock = {
+    
+    Param (
+        $VMName,
+        $VMRGName
+        
+    )
+
+
+$vmdetails= Get-AzVM -ResourceGroupName $VMRGName -Name $VMName
+
+$NicName = $vmdetails.NetworkProfile.NetworkInterfaces[0].Id
+
+$arr= $NicName -split '/'
+
+$NicName1 = $arr[8]
+
+$Nic = Get-AzNetworkInterface -ResourceGroupName $VMRgName -Name $NicName1
+
+$nic.EnableAcceleratedNetworking = $true
+
+$nic | Set-AzNetworkInterface
+
+
+       
+}
+
+
+foreach($VMName in $VMList)
+
+{
+
+
+$Jobs += start-job -ScriptBlock $sblock -ArgumentList $VMName,$VMRGName
+
+Write-Host "  "
+
+Write-host "Enabling Accelerated Networking on $($VMName) ..."
+
+Write-Host "  "
+
+
+}
+
+$jobs | Wait-Job | Receive-Job
