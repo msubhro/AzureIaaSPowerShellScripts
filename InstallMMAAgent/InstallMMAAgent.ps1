8f37c9a41a191fcd3f59d93f689f4275a3244cc3
Subhro Majumder <48013811+msubhro@users.noreply.github.com>
2020-06-21 14:19:56 +0530

    Add files via upload

---
+++ InstallMMAAgent/InstallMMAAgent.ps1
@@ -0,0 +1,78 @@
+﻿
+# This script install the Azure VM Extension for Microsoft Monitoring Agent (MMA).
+
+# The script also connects Azure VM to a Log Analytics Workspace.
+
+# Works on multiple Azure VMs in parallel, based on an input file.
+
+# This script runs on PowerShell AZ Module.
+
+# The script is tested only for Windows VM.
+
+# Test before you use this script in a critical environment.
+
+# Use at your own risk.
+
+
+# Log Analytics Workspace ID
+
+$WorkspaceId = ' '
+
+# Log Analytics Workspace Key
+
+$workspaceKey =' '
+
+$VMList = Get-Content "c:\serverlist.txt"
+
+$VMRGName = 'TestVMRG1'
+
+$VMLocation = 'NorthEurope'
+
+$Jobs = @()
+
+
+$sblock = {
+    
+    Param (
+        $WorkspaceId,
+        $workspaceKey,
+        $VMName,
+        $VMRGName,
+        $VMLocation
+    )
+
+    $PublicSettings = @{"workspaceId" = $WorkspaceId}
+    $ProtectedSettings = @{"workspaceKey" = $workspaceKey}
+
+    Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" `
+    -ResourceGroupName $VMRGName `
+    -VMName $VMName `
+    -Publisher "Microsoft.EnterpriseCloud.Monitoring" `
+    -ExtensionType "MicrosoftMonitoringAgent" `
+    -TypeHandlerVersion 1.0 `
+    -Settings $PublicSettings `
+    -ProtectedSettings $ProtectedSettings `
+    -Location $VMLocation
+    
+       
+}
+
+
+
+foreach($VMName in $VMList)
+
+{
+
+
+$Jobs += start-job -ScriptBlock $sblock -ArgumentList $WorkspaceId,$workspaceKey,$VMName,$VMRGName,$VMLocation
+
+Write-Host "  "
+
+Write-host "Installing MMA Agent on $($VMName) ..."
+
+Write-Host "  "
+
+
+}
+
+$jobs | Wait-Job | Receive-Job
